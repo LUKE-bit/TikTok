f7ae26c95393fdeedd1af5b3ee164d253962fad5
LUKE-bit <57066589+LUKE-bit@users.noreply.github.com>
2022-06-09 17:27:24 +0800

    Update favorite.go

--- controller/favorite.go
+++ controller/favorite.go
@@ -1,17 +1,33 @@
 package controller
 
 import (
+	"TikTok/service"
 	"TikTok/utils"
-	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
 )
 
 // FavoriteAction no practical effect, just check if token is valid
 func FavoriteAction(c *gin.Context) {
 	token := c.Query("token")
+	videoId, _ := strconv.ParseInt(c.Query("video_id"), 10, 64)
+	actionType, _ := strconv.ParseInt(c.Query("action_type"), 10, 64)
 	if token != "" {
 		if claim, err := utils.ParseToken(token); claim != nil && err == nil {
-			c.JSON(http.StatusOK, Response{StatusCode: 0})
+			err = service.AddLike(videoId, actionType)
+			if err != nil {
+				c.JSON(http.StatusOK, Response{
+					StatusCode: 1,
+					StatusMsg:  err.Error(),
+				})
+			} else {
+				c.JSON(http.StatusOK, Response{
+					StatusCode: 0,
+					StatusMsg:  "AllRight",
+				})
+			}
 			return
 		}
 	}
@@ -20,10 +36,17 @@ func FavoriteAction(c *gin.Context) {
 
 // FavoriteList all users have same favorite video list
 func FavoriteList(c *gin.Context) {
-	c.JSON(http.StatusOK, VideoListResponse{
-		Response: Response{
-			StatusCode: 0,
-		},
-		VideoList: DemoVideos,
-	})
+	token := c.Query("token")
+	if token != "" {
+		if claim, err := utils.ParseToken(token); claim != nil && err == nil {
+			video_list := service.GetFavoriteList()
+			c.JSON(http.StatusOK, VideoListResponse{
+				Response: Response{
+					StatusCode: 0,
+				},
+				VideoList: video_list,
+			})
+		}
+	}
+
 }
